5184436b068e3529fc9b4251434a2806295f9836
ElhalwaguiAhmedMohamed <elhalwaguiahmedmohamed@gmail.com>
2022-03-19 21:01:40 +0200

    service router validation

--- routers/serviceRouter.js
+++ routers/serviceRouter.js
@@ -4,6 +4,26 @@ const controller = require("../controllers/serviceController");
 
 const router = express.Router();
 
-router.get("", controller.getAllServices).post("", controller.createService);
-router.get("/branches/:serviceId", controller.getServiceBranches);
+router
+  .get("", controller.getAllServices)
+  .post(
+    "",
+    [
+      body("name").isString().notEmpty().withMessage("name should be string"),
+      body("branches").isArray().withMessage("branches is an array"),
+      body("branches.*")
+        .isAlphanumeric()
+        .withMessage("branches is array of alphaumerics"),
+    ],
+    controller.createService
+  );
+router.get(
+  "/branches/:serviceId",
+  [
+    param("serviceId")
+      .isAlphanumeric()
+      .withMessage("serviceId should be alphanumeric"),
+  ],
+  controller.getServiceBranches
+);
 module.exports = router;
