692e580b30b750fe99f1ca969d5a1f3561412c48
Sarrah Ashraf Khamis <sarrahashrafkhamis@gmail.com>
2022-03-20 18:00:23 +0200

    complete doctor CRUD

--- controllers/doctorController.js
+++ controllers/doctorController.js
@@ -27,7 +27,7 @@ exports.getDoctorsWithService = (req, res, next) => {
   }
   user
     .find({ role: "doctor" })
-    .populate({ path: "owner", model:'doctor'})
+    .populate({ path: "owner", model: "doctor" })
     .then((data) => {
       let docsUsers = data.filter(
         (docUser) => docUser.owner.specialization == req.params.specId
@@ -52,7 +52,7 @@ exports.getDoctorSchedule = (req, res, next) => {
   }
   user
     .findById(req.params.docUserId)
-    .populate({ path: "owner", model:'doctor'})
+    .populate({ path: "owner", model: "doctor" })
     .then((data) => {
       res.status(200).json(data.owner.schedule);
     })
@@ -74,7 +74,7 @@ exports.createDoctor = (req, res, next) => {
   }
   new doctor({
     specialization: req.body.specId,
-    schedule: req.body.schedule
+    schedule: req.body.schedule,
   })
     .save()
     .then((doc) => {
@@ -86,10 +86,59 @@ exports.createDoctor = (req, res, next) => {
         owner: doc._id,
       })
         .save()
-        .then((data) => res.status(200).json({message:'added', data:data}))
+        .then((data) => res.status(200).json({ message: "added", data: data }))
         .catch((err) => {
           console.log(err);
           next(err);
         });
     });
+};
+
+exports.updateDoctor = (req, res, next) => {
+  let errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    let error = new Error();
+    error.status = 422;
+    error.message = errors
+      .array()
+      .reduce((current, object) => current + object.msg + " ", "");
+    throw error;
+  }
+  user
+    .findByIdAndUpdate(req.body.docUserId, {
+      $set: {
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+      },
+    })
+    .then((data) => {
+      if (data == null) throw new Error("DoctorUser is not found!");
+      doctor
+        .findByIdAndUpdate(data.owner, {
+          $set: {
+            specId: req.body.specId,
+            schedule: req.body.schedule,
+          },
+        })
+        .then((doc) => {
+          res
+            .status(200)
+            .json({ message: "updated", data: { user: data, doctor: doc } });
+        })
+        .catch((err) => next(err));
+    })
+    .catch((err) => next(err));
+};
+
+exports.deleteDoctor = (req, res, next) => {
+  user
+    .findByIdAndDelete(req.params.docUserId)
+    .then((data) => {
+      doctor
+        .findByIdAndDelete(data.owner)
+        .then((doc) => res.status(200).json({ message: "deleted" }))
+        .catch((err) => next(err));
+    })
+    .catch((err) => next(err));
 };
