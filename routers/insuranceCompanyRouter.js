d7f93a1b6c1b7e9a6f874a3152aab078df491208
Sarrah Ashraf Khamis <sarrahashrafkhamis@gmail.com>
2022-03-23 04:01:22 +0200

    add expenses => insurance company controller

--- routers/insuranceCompanyRouter.js
+++ routers/insuranceCompanyRouter.js
@@ -1,8 +1,12 @@
-const express = require('express');
-const {body,param}= require('express-validator');
-const insuranceCompanyController= require('../controllers/insuranceCompanyController');
+const express = require("express");
+const { body, param } = require("express-validator");
+const insuranceCompanyController = require("../controllers/insuranceCompanyController");
 const router = express.Router();
-router.get('/',insuranceCompanyController.getInsuranceCompany)
-router.post('/',insuranceCompanyController.postInsuranceCompany)
-router.delete('/:insuranceCompanyId',insuranceCompanyController.deleteInsuranceCompany)
-module.exports= router;
+router.get("/", insuranceCompanyController.getInsuranceCompany);
+router.post("/", insuranceCompanyController.postInsuranceCompany);
+router.delete(
+  "/:insuranceCompanyId",
+  insuranceCompanyController.deleteInsuranceCompany
+);
+router.put("/:insuranceCompanyId", insuranceCompanyController.addExpenses);
+module.exports = router;
