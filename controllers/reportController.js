138481892609be1f4f0beb1283f1e483c33d5b2a
aalaaFathallah <aalaa.fathallah2@gmail.com>
2022-03-25 22:25:05 +0200

    getInvoicesSummary

--- controllers/reportController.js
+++ controllers/reportController.js
@@ -1,6 +1,8 @@
 const { validationResult } = require("express-validator");
 const patientModel = require("../models/patientSchema")
-exports.getGenderSummary = async (req,res,next) => {
+const invoiceModel = require("../models/invoiceSchema")
+const appointmentModel = require("../models/appointmentSchema");
+exports.getGenderSummary = async (req, res, next) => {
   let errors = validationResult(req);
   if (!errors.isEmpty()) {
     let error = new Error();
@@ -9,15 +11,37 @@ exports.getGenderSummary = async (req,res,next) => {
       .array()
       .reduce((current, object) => current + object.msg + " ", "");
     next(error);
+  } else {
+    let male = 0;
+    let female = 0;
+    (await patientModel.find({})).forEach(patient => {
+      (patient.gender === 'male') ? male++ : female++
+    })
+    res.status(200).json({ male, female })
   }
-  let male = 0;
-  let female = 0;
-  (await patientModel.find({})).forEach(patient => {
-    (patient.gender === 'male') ? male++ : female++
-  })
-  res.status(200).json({male,female})
+
 }
-exports.getInvoicesSummary = () => {
+exports.getInvoicesSummary = (req, res, next) => {
+  let errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    let error = new Error();
+    error.status = 422;
+    error.message = errors
+      .array()
+      .reduce((current, object) => current + object.msg + " ", "");
+    next(error);
+  } else {
+    let appointment = {}
+    appointmentModel.find({}).populate('invoice').then(appointments => {
+      appointments.forEach(app => {
+       // appDateMonth = new Date(app.date).getMonth();
+        const appDateMonth = new Date(app.date).toLocaleString('default', { month: 'long' });
+        appointment[appDateMonth] = appointment[appDateMonth] ? appointment[appDateMonth] + app.invoice.paidAmount : app.invoice.paidAmount
+      })
+      res.status(200).json({ appointment })
+
+    })
+  }
 
 }
 exports.getInvoices = () => { }
