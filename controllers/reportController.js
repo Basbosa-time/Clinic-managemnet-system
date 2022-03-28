ead047980b7505795dc976b775a32fd14b37b0d0
aalaaFathallah <aalaa.fathallah2@gmail.com>
2022-03-28 18:02:05 +0200

    enhanced getInvoicesSummary

--- controllers/reportController.js
+++ controllers/reportController.js
@@ -31,14 +31,16 @@ exports.getInvoicesSummary = (req, res, next) => {
       .reduce((current, object) => current + object.msg + " ", "");
     next(error);
   } else {
-    let appointment = {}
+    let appointment = {'January':0,'February':0,'March':0,'April':0,'May':0,'June':0,'July':0,'August':0,'september':0,'October':0,'November':0,'December':0}
     appointmentModel.find({}).populate('invoice').then(appointments => {
       appointments.forEach(app => {
         // appDateMonth = new Date(app.date).getMonth();
         const appDateMonth = new Date(app.date).toLocaleString('default', { month: 'long' });
         appointment[appDateMonth] = appointment[appDateMonth] ? appointment[appDateMonth] + app.invoice.paidAmount : app.invoice.paidAmount
       })
-      res.status(200).json(appointment)
+      monthes=Object.keys(appointment)
+      data = Object.values(appointment)
+      res.status(200).json({data,monthes})
     })
   }
 
@@ -53,7 +55,7 @@ exports.getInvoices = (req, res, next) => {
       .reduce((current, object) => current + object.msg + " ", "");
     next(error);
   } else {
-    invoiceModel.find({}).then(invoices => {
+    invoiceModel.find({}).populate({path:"recep",model:"user",select:"name"}).then(invoices => {
       res.status(200).json(invoices)
     })
 
